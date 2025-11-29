94108c55bb61247953af2cad1d827aa76714d579
Pedro1santiago <pedro.santigosiqueira@gmail.com>
2025-11-29 00:47:24 -0300

    "Fix base for Vercel and adjust build"

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // ✅ importante para Vercel
+  base: "./", 
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", 
   },
 });
